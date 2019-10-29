573041819726d24aebdd6cd083951625331ac67e
Niklas Fondberg <niklas.fondberg@photowall.se>
2019-10-29 09:04:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  console.log('Got request', JSON.stringify(req.connection.remoteAddress, null, 2));
+  console.log('Env', JSON.stringify(process.env, null, 2));
+  res.send('Hello world 12345\n' + process.env.DBURL + ' - ' + process.env.PARAMTEST1 + '\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+console.log('DBURL\n' + process.env.DBURL + ' - PARAMTEST1:' + process.env.PARAMTEST1 + '\n');
