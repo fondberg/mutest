ffae1cbe7b0385301e0c06cc753e70d2f9b5d243
Niklas Fondberg <niklas.fondberg@photowall.se>
2019-11-05 09:33:03 +0100

    Added db

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const express = require('express');
+const { Client } = require('pg');
 
 // Constants
-const PORT = 80;
+const PORT = 8011;
 const HOST = '0.0.0.0';
 
 // App
@@ -11,7 +12,28 @@ const app = express();
 app.get('/', (req, res) => {
   console.log('Got request', JSON.stringify(req.connection.remoteAddress, null, 2));
   console.log('Env', JSON.stringify(process.env, null, 2));
-  res.send('Hello world Thu 15:27 under development\n' + process.env.DBURL + ' - ' + process.env.PARAMTEST1 + ' - ' + process.env.PARAMTEST2 + '\n');
+  res.send('Hello world staging Thu 15:27 under development\n' + process.env.DBURL + ' - ' + process.env.PARAMTEST1 + ' - ' + process.env.PARAMTEST2 + '\n');
+});
+
+
+app.get('/pg', (request, response) => {
+  const client = new Client({
+    connectionString: process.env.DBURL// e.g. postgres://user:password@host:5432/database
+  });
+  client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+      response.status(503).json(err.stack);
+    } else {
+      console.log('connected');
+    }
+  });
+  client.query('SELECT NOW()', (err, res) => {
+    if (err) throw err
+    console.log(res);
+    response.status(200).json(res);
+    client.end();
+  });
 });
 
 app.listen(PORT, HOST);
